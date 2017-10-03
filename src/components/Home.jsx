e340c0210091eff9e3d92dd8e0c6ab3f6f9e0cb2
administrator <liquidnuker@users.noreply.github.com>
2017-10-02 19:20:59 -0700

    rkInfoSpan101 mounted

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,18 @@
+import RkInfoSpan101 from "./RkInfoSpan101.jsx"
+
 export default function Home(props) {
   return (
-    <div class="row">    
-      <div class="col-sm-12">
-        Home
+    <div>
+
+  {/*rkInfoSpan101*/}
+    <h2 className="item-id">rkinfoSpan101</h2>
+    <div className="row">
+      <div className="col-sm-3">39</div>
+      <div className="col-sm-9">
+      <RkInfoSpan101 />
       </div>
-    </div>  
+    </div>
+
+    </div>
   );
 }
